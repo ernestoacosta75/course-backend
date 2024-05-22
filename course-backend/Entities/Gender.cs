ae83ac0d1efebf4ec2c83a33196e5b6b08718777
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2024-05-22 14:17:46 +0200

    Custom validations

--- course-backend/Entities/Gender.cs
+++ course-backend/Entities/Gender.cs
@@ -1,12 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using course_backend.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace course_backend.Entities;
 
-public class Gender
+public class Gender : IValidatableObject
 {
     public int Id { get; set; }
     
     [Required]
     [StringLength(maximumLength: 10)]
+    //[FirstLetterUppercase]
     public string Name { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(Name))
+        {
+            var firstLetter = Name[0].ToString();
+
+            if (firstLetter != firstLetter.ToUpper())
+            {
+                yield return new ValidationResult("The first letter must be uppercase",
+                    new string[] { nameof(Name) });
+            }
+        }
+    }
 }
