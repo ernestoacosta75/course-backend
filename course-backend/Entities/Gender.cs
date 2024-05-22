e7d33c5f471a19e020fd0dd6c4e76b445ed1d366
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2024-05-22 13:59:30 +0200

    Validations

--- course-backend/Entities/Gender.cs
+++ course-backend/Entities/Gender.cs
@@ -1,7 +1,12 @@
-﻿namespace course_backend.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace course_backend.Entities;
 
 public class Gender
 {
     public int Id { get; set; }
+    
+    [Required]
+    [StringLength(maximumLength: 10)]
     public string Name { get; set; }
 }
