10c5c1c7d2783263328ceabf9beed31b24221705
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2024-06-08 17:40:34 +0200

    Adding documentation cor the extensions

--- course-backend/Utilities/HttpContextExtensions.cs
+++ course-backend/Utilities/HttpContextExtensions.cs
@@ -2,6 +2,10 @@
 
 namespace course_backend.Utilities
 {
+    /// <summary>
+    /// This extension will be used to paginate and return back to the frontend
+    /// the quantity of records that exists in the database for the corresponding resource (tables).
+    /// </summary>
     public static class HttpContextExtensions
     {
         public async static Task InsertPaginationParametersInHeader<T>(this HttpContext httpContext,
--- course-backend/Utilities/IQueryableExtensions.cs
+++ course-backend/Utilities/IQueryableExtensions.cs
@@ -2,6 +2,10 @@
 
 namespace course_backend.Utilities
 {
+    /// <summary>
+    /// Since the pagination can occurs from different places,
+    /// this extension will centralize this kind of operation in EF Core.
+    /// </summary>
     public static class IQueryableExtensions
     {
         public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable,
