33a5a8754d3ddfdfff7ebc5c2cb5d843a0fcd1a7
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2024-06-18 16:28:49 +0200

    Refactoring code

--- Films.Infrastructure/DependencyResolver/ServiceCollectionExtensions.cs
+++ Films.Infrastructure/DependencyResolver/ServiceCollectionExtensions.cs
@@ -2,12 +2,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using Films.Core.DomainServices.Repositories;
 using Castle.DynamicProxy;
-using Films.Core.DomainServices.DatabaseContext;
+using Films.Core.DomainServices.DependencyResolver;
 using Films.Infrastructure.Aspects;
 using Films.Core.DomainServices.UnitOfWorks;
 using Films.Infrastructure.UnitOfWorks;
 using Films.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
 
 namespace Films.Infrastructure.DependencyResolver
 {
@@ -15,25 +14,11 @@ namespace Films.Infrastructure.DependencyResolver
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            // services.AddDatabaseContext(configuration);
-            var isHomeConnection = configuration.GetValue<bool>("isHomeConnection");
-
-            string? connectionString = isHomeConnection ?
-                configuration.GetConnectionString("DefaultConnection") :
-                configuration.GetConnectionString("VdiConnection");
-
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                throw new InvalidOperationException("No connection string found in configuration.");
-            }
-
-            services.AddDbContext<FilmsDbContext>(options =>
-                    options.UseSqlServer(connectionString), ServiceLifetime.Scoped);
+            services.AddDatabaseContext(configuration);
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             
-
             services.AddSingleton(new ProxyGenerator());
             services.AddScoped<IInterceptor, LoggingInterceptor>();
         }
